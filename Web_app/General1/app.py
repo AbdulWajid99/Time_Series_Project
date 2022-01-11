856fc6d6fa925238ccf4100f6f6e530ab38bc92a
AbdulWajid99 <wajid.linux99@gmail.com>
2022-01-11 15:59:55 +0500

    Update

--- Web_app/General1/app.py
+++ Web_app/General1/app.py
@@ -14,7 +14,9 @@ import numpy as np
 app = Flask(__name__)  # indicate where to start the app # return script name
 
 # read data path
-df = pd.read_csv("\Future_results.csv")
+df = pd.read_csv(
+    "E:\Data_Scientist\Python\Projects\Machine_Learning_Projects\Time_Series_Project-Hassan\Web_app\General1\Future_results1.csv"
+)
 
 
 @app.route("/")  # indicating root
@@ -26,32 +28,73 @@ def main():
 def home():
     # save returened value
     data1 = request.form["month"]
-
+    result_not_available = "Prediction not predicted yet"
+    
     # select value
     if data1 == "2022-01":
-        result = int(df.iloc[36, 3])
+        try:
+            result = int(df.iloc[36, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-02":
-        result = int(df.iloc[37, 3])
+        try:
+            result = int(df.iloc[37, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-03":
-        result = int(df.iloc[38, 3])
+        try:
+            result = int(df.iloc[38, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-04":
-        result = int(df.iloc[39, 3])
+        try:
+            result = int(df.iloc[39, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-05":
-        result = int(df.iloc[40, 3])
+        try:
+            result = int(df.iloc[40, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-06":
-        result = int(df.iloc[41, 3])
+        try:
+            result = int(df.iloc[41, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-07":
-        result = int(df.iloc[42, 3])
+        try:
+           result = int(df.iloc[42, 3])
+        except:
+            result = result_not_available
+        
     elif data1 == "2022-08":
-        result = int(df.iloc[43, 3])
+        try:
+            result = int(df.iloc[43, 3])
+        except:
+            result = result_not_available
+        
     elif data1 == "2022-09":
-        result = int(df.iloc[44, 3])
+        try:
+            result = int(df.iloc[44, 3])
+        except:
+            result = result_not_available
+        
     elif data1 == "2022-10":
-        result = int(df.iloc[45, 3])
+        try:
+            result = int(df.iloc[45, 3])
+        except:
+            result = result_not_available
+
     elif data1 == "2022-11":
-        result = int(df.iloc[46, 3])
+        try:
+            result =  int(df.iloc[46, 3])
+        except:
+            result = result_not_available
     elif data1 == "2022-12":
-        result = int(df.iloc[47, 3])
+        try:
+            result = int(df.iloc[47, 3])
+        except:
+            result = result_not_available
     else:
         result = "Prediction not predicted yet"
 
